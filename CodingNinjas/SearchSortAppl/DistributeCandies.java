4d52aabc1ee64b2e9d4b8d3c01acda5d262fe1a5
hardik <hardik96bansal@gmail.com>
2020-04-04 18:00:57 +0530

    Candies distributed

--- CodingNinjas/SearchSortAppl/DistributeCandies.java
+++ CodingNinjas/SearchSortAppl/DistributeCandies.java
@@ -4,6 +4,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class DistributeCandies{
+
+    public static long callMe(long[] arr, long start, long end, int k){
+        long curr = 0;
+        if(end<=start){
+            return start;
+        }
+        if(end-start == 1){
+            long curr1 = 0;
+            long curr2 = 0;
+
+            for(int i=0;i<arr.length;i++){
+                curr1 = curr1+arr[i]/start;
+            }
+            for(int i=0;i<arr.length;i++){
+                curr2 = curr2+arr[i]/end;
+            }
+            if(curr2>=k){
+                return end;
+            }
+            return start;
+
+        }
+        long mid = (start+end)/2;
+
+        for(int i=0;i<arr.length;i++){
+            curr = curr+arr[i]/mid;
+        }
+        if(curr>=k){
+            return callMe(arr, mid, end, k);
+        }
+        else{
+            return callMe(arr, start, mid, k);
+        }
+
+    }
     
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
@@ -14,18 +49,26 @@ class DistributeCandies{
             int k = scn.nextInt();
             long[] arr = new long[n];
 
-            for(int i=0;i<n;i++){
-                arr[i] = scn.nextLong();                
-            }
-            Arrays.sort(arr);
-            if(k<=n){
-                System.out.println(arr[n-k]);
-            }
-            else{
-                
-            }
-            
 
+            long max = Long.MIN_VALUE;
+            long min = Long.MAX_VALUE;
+
+            for(int i=0;i<n;i++){
+                arr[i] = scn.nextLong();     
+                if(arr[i]>max){
+                    max = arr[i];
+                }           
+                if(arr[i]<min){
+                    min = arr[i];
+                }
+            }
+
+            long ans = callMe(arr, 0, max, k);
+
+            System.out.println(ans);
+
+
+            
             t--;
         }
     }
