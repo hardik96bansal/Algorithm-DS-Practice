3862640b459c4bd3b2b8458f640ea654f955c115
hardik <hardik96bansal@gmail.com>
2020-04-22 23:33:36 +0530

    dpdpdpdpdp

---
+++ CodingNinjas/DynamicProgramming1/MagicGrid.java
@@ -0,0 +1,72 @@
+package DynamicProgramming1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagicGrid {
+
+    public static int callMe(int[][] arr, int si, int sj, int ei, int ej, int[][] reff){
+        if(si==ei && sj==ej){
+            return 1;
+        }
+        if(si>ei){
+            return Integer.MIN_VALUE;
+        }
+        if(sj>ej){
+            return Integer.MIN_VALUE;
+        }
+        if(reff[si][sj]!=-1) return reff[si][sj];
+
+        int first = callMe(arr, si, sj+1, ei, ej, reff);
+        int second = callMe(arr, si+1, sj, ei, ej, reff);
+        int ans = 0;
+
+        if(first==Integer.MIN_VALUE){
+            second = second - arr[si][sj];
+            if(second<=0) second = 1;
+            ans = second;
+        }
+        else if(second==Integer.MIN_VALUE){
+            first = first - arr[si][sj];
+            if(first<=0) first = 1;
+            ans = first;
+        } 
+        else{
+            first = first - arr[si][sj];
+            second = second - arr[si][sj];
+            if(first<=0) first = 1;
+            if(second<=0) second = 1;
+            ans = Math.min(first, second);
+        }
+        
+        reff[si][sj] = ans;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int t = scn.nextInt();
+
+        while(t>0){
+            int r = scn.nextInt();
+            int c = scn.nextInt();
+            int[][] arr = new int[r][c];
+            int[][] reff = new int[r+1][c+1];
+
+            for(int i=0;i<r;i++){
+                for(int j=0;j<c;j++){
+                    arr[i][j] = scn.nextInt();
+                }
+            }
+
+            for(int[] array : reff){
+                Arrays.fill(array, -1);
+            }
+            
+            System.out.println(""+callMe(arr, 0, 0, r-1, c-1, reff));
+            t--;
+        }
+    }
+
+}
---
+++ CodingNinjas/DynamicProgramming1/MinCostGrid.java
@@ -0,0 +1,39 @@
+package DynamicProgramming1;
+
+import java.util.Arrays;
+
+public class MinCostGrid {
+
+    public static int minCost(int[][] arr,int si,int sj,int ei,int ej, int[][] reff){
+        if(si==ei && sj==ej) return arr[ei][ej];
+        if(si>ei || sj>ej) return Integer.MAX_VALUE;
+
+        if(reff[si][sj]!=-1) return reff[si][sj];
+
+        int op1 = minCost(arr, si+1, sj, ei, ej, reff);
+        int op2 = minCost(arr, si, sj+1, ei, ej, reff);
+        int op3 = minCost(arr, si+1, sj+1, ei, ej, reff);
+
+        return arr[si][sj]+Math.min(op1, Math.min(op2, op3));
+    }
+
+    public static void main(String[] args) {
+        int[][] input = new int [3][3];
+        input[0][0] = 4;
+        input[0][1] = 3;
+        input[0][2] = 2;
+        input[1][0] = 1;
+        input[1][1] = 8;
+        input[1][2] = 3;
+        input[2][0] = 1;
+        input[2][1] = 1;
+        input[2][2] = 8;
+
+        int[][] reff = new int[4][4];
+        for(int[] arr:reff){
+            Arrays.fill(arr, -1);
+        }
+        System.out.println(""+minCost(input, 0, 0, 2, 2, reff));
+        
+    }
+}
