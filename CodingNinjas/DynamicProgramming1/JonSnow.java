7fa7a465024ac6a2b9e738fc19e41efe29879b3f
hardik <hardik96bansal@gmail.com>
2020-05-01 19:28:11 +0530

    Jon Snow kahika

---
+++ CodingNinjas/DynamicProgramming1/JonSnow.java
@@ -0,0 +1,32 @@
+package DynamicProgramming1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JonSnow {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        int x = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+
+        for(int j=0;j<k;j++){
+            Arrays.sort(arr);
+            for(int i=0;i<n;i=i+2){
+                arr[i] = arr[i] ^ x;
+            }
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n-1]+" "+ arr[0]);
+    }
+
+}
---
+++ CodingNinjas/DynamicProgramming1/JonSnow1.java
@@ -0,0 +1,72 @@
+package DynamicProgramming1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JonSnow1 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        int x = scn.nextInt();
+
+        int[] arr = new int[n];
+        int[] reff = new int[2000];
+        int[] temp = new int[2000];
+        Arrays.fill(reff, 0);
+        Arrays.fill(temp, 0);
+
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+            reff[arr[i]]++;
+        }
+
+        for(int i=0;i<k;i++){
+            int counted = 0;
+            for(int j=0;j<1025;j++){
+                if(reff[j]==0) continue;
+                int target = j^x;
+                int tAdd = 0;
+                int cAdd = 0;
+                if(j%2==0){
+                    tAdd = reff[j]/2;
+                    cAdd = reff[j]/2;
+                }
+                else{
+                    if(counted%2==0){
+                        tAdd = reff[j]/2+1;
+                        cAdd = reff[j]/2;
+                    }
+                    else{
+                        tAdd = reff[j]/2;
+                        cAdd = reff[j]/2+1;
+                    }
+                }
+                temp[j] += cAdd;
+                temp[target] += tAdd;
+                counted += reff[j];
+            }
+            reff = Arrays.copyOf(temp, 2000);
+            Arrays.fill(temp, 0);
+        }
+
+        int big = 0, small = 0;
+
+        for(int i=0;i<1025;i++){
+            if(reff[i]!=0){
+                small = i;
+                break;
+            }
+        }
+        for(int i=1025;i>=0;i--){
+            if(reff[i]!=0){
+                big = i;
+                break;
+            }
+        }
+
+        System.out.println(big+" "+small);
+    }
+
+}
