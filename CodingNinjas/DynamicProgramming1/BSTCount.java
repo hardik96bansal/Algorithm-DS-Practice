2bae2b121a3307b7223fdd71a1a4d5dd01c05466
hardik <hardik96bansal@gmail.com>
2020-05-03 17:46:35 +0530

    More blehhh

---
+++ CodingNinjas/DynamicProgramming1/BSTCount.java
@@ -0,0 +1,59 @@
+package DynamicProgramming1;
+
+import java.util.Arrays;
+
+class BSTCount{
+    public static int addModulo(int a, int b){
+        long bignum = 1000000000+7;
+        long temp = a%bignum + b%bignum;
+        temp = temp%bignum;
+        return (int)temp;
+    }
+    
+    public static int subModulo(int a, int b){
+        long bignum = 1000000000+7;
+        long temp = a%bignum - b%bignum;
+        temp = temp%bignum;
+        return (int)temp;
+    }
+    
+    public static int prodModulo(int a, int b){
+        long bignum = 1000000000+7;
+        long temp = a%bignum * b%bignum;
+        temp = temp%bignum;
+        return (int)temp;
+    }
+
+    public static int countTreesUtil(int start, int count, int[][] arr){
+        if(count <=1){
+            return 1;
+        }
+        if(count==2){
+            return 2;
+        }
+        int res = 0;
+        if(arr[start][count]!=-1) return arr[start][count];
+        for(int i=1;i<=count;i++){
+            int currRoot = subModulo(addModulo(start, i),1);
+            if(currRoot-start==1 && count-i==1) res= addModulo(res, 1);
+            else{
+                int ans1 = countTreesUtil(start, currRoot-start, arr);
+                int ans2 = countTreesUtil(start+i, count-i, arr);
+                int ans = prodModulo(ans1,ans2);
+                res = addModulo(res, ans);
+            }
+        }
+
+        arr[start][count] = res;
+        return res;
+    }
+    public static int countTrees(int numKeys) {
+        if(numKeys==1) return 1;
+        int[][] arr = new int[numKeys+1][numKeys+1];
+        for(int[] temp: arr){
+            Arrays.fill(temp, -1);
+        }
+        return countTreesUtil(1, numKeys, arr);
+    }
+    
+}
---
+++ CodingNinjas/DynamicProgramming1/MinChocolates.java
@@ -0,0 +1,64 @@
+package DynamicProgramming1;
+
+import javax.management.RuntimeErrorException;
+
+public class MinChocolates {
+    public static int callMe(int[] arr, int n, int curr, int chain, int[] reff){
+        if(curr==n-1){
+            reff[curr] = chain;
+            return chain;
+        }
+        if(arr[curr]<arr[curr+1]){
+            reff[curr] = chain;
+            int op1 = callMe(arr, n, curr+1, chain+1, reff);
+            if(reff[curr]>=op1){
+                reff[curr] = op1-1;
+            }
+            return reff[curr];
+        }
+        if(arr[curr]>arr[curr+1]){
+            reff[curr] = chain;
+            int op1 = callMe(arr, n, curr+1, 1, reff);
+            if(reff[curr]<=op1){
+                reff[curr] = op1+1;
+            }
+            return reff[curr];
+        }
+        if(arr[curr]==arr[curr+1]){
+            if(curr+1==n-1){
+                reff[curr+1] = 1;
+                reff[curr] = chain;
+                return reff[curr];
+            }
+            else{
+                reff[curr] = chain;
+                reff[curr+1] = callMe(arr, n, curr+1, 1, reff);
+                return reff[curr];
+            }
+        }
+
+        return 1;       
+    }
+    public static int getMin(int arr[], int N){
+        int[] reff = new int[N+1];
+        
+        callMe(arr, N, 0, 1, reff);
+        int ans = 0;
+
+        for(int i=0;i<N;i++){
+            ans+=reff[i];
+        }
+
+        return ans;
+    }
+    
+
+    public static void main(String[] args) {
+        int[] arr = {82440, 36222, 39726, 90098, 21130, 95534, 90800, 97143, 56966, 45758, 27836, 30506, 59211, 72381, 5674, 45858, 85108, 10284, 93404, 66620};
+        int[] arr1 = {};
+        System.out.println(getMin(arr1, 0));
+
+        
+    }
+
+}
