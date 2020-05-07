e1c03b69f20b71c5db1f3a0389e74857677a7c0c
hardik <hardik96bansal@gmail.com>
2020-05-07 22:59:52 +0530

    DP2

---
+++ CodingNinjas/DynamicProgramming2/EditDistance.java
@@ -0,0 +1,84 @@
+package DynamicProgramming2;
+
+import java.util.Arrays;
+
+public class EditDistance {
+
+    public static int lcs(String s1, String s2, int remS1L, int remS2L, int[][] arr){
+        if(s1.length()==0||s2.length()==0){
+            return 0;
+        }
+
+        if(arr[remS1L][remS2L]!=-1){
+            return arr[remS1L][remS2L];
+        }
+        
+        int ans = 0;
+        if(s1.charAt(0)==s2.charAt(0)){
+            ans = 1+ lcs(s1.substring(1), s2.substring(1), remS1L-1, remS2L-1, arr);
+        }
+        else{
+            int opt1 = lcs(s1.substring(1), s2, remS1L-1, remS2L, arr);
+            int opt2 = lcs(s1, s2.substring(1), remS1L, remS2L-1, arr);
+            ans = Math.max(opt1, opt2);
+        }
+        arr[remS1L][remS2L] = ans;
+        return ans;
+
+    }
+
+
+    public static int callMe(String s1, String s2, int m, int n, int[][] arr){
+        if(s1.length()==0 && s2.length()==0){
+            return 0;
+        }
+        if(s1.length()==0){
+            return s2.length();
+        }
+        if(s2.length()==0){
+            return s1.length();
+        }
+
+        if(arr[m][n]!=-1){
+            return arr[m][n];
+        }
+
+        int ans = 0;
+        if(s1.charAt(0)==s2.charAt(0)){
+            ans = callMe(s1.substring(1), s2.substring(1), m-1, n-1, arr);
+        }
+        else{
+            int opt1 = 1 + callMe(s1.substring(1), s2, m-1, n, arr);
+            int opt2 = 1 + callMe(s1, s2.substring(1), m, n-1, arr);
+            int opt3 = 1 + callMe(s1.substring(1), s2.substring(1), m-1, n-1, arr);
+            if(opt1<=opt2 && opt1<=opt3){
+                ans = opt1;
+            }
+            else if(opt2<=opt1 && opt2<=opt3){
+                ans = opt2;
+            }
+            else if(opt3<=opt2 && opt3<=opt1){
+                ans = opt3;
+            }
+        }
+
+        arr[m][n] = ans;
+        return ans;
+    }
+
+    public static int editDistance(String s1, String s2){
+        int remS1L = s1.length();
+        int remS2L = s2.length();
+        int[][] arr = new int[remS1L+1][remS2L+1];
+        for(int[] temp : arr){
+            Arrays.fill(temp, -1);
+        }
+        int ans = callMe(s1, s2, remS1L, remS2L, arr);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(editDistance("abcd", "acbd"));
+        
+    }
+}
---
+++ CodingNinjas/DynamicProgramming2/LongComSubRec.java
@@ -0,0 +1,75 @@
+package DynamicProgramming2;
+
+import java.util.Arrays;
+
+public class LongComSubRec {
+
+    public static int lcs(String a, String b, int al, int bl, int[][] arr){
+        if(a.length()==0 || b.length()==0){
+            return 0;
+        }
+
+        if(arr[al][bl] != -1){
+            return arr[al][bl];
+        }
+        int ans = 0;
+        if(a.charAt(0) == b.charAt(0)){
+            ans = 1+lcs(a.substring(1), b.substring(1), al-1, bl-1, arr);
+        }
+        else{
+            int opt1 = lcs(a.substring(1), b, al-1, bl, arr);
+            int opt2 = lcs(a, b.substring(1), al, bl-1, arr);
+            ans = Math.max(opt1, opt2);
+        }
+        arr[al][bl] = ans;
+        return ans;
+    }
+
+    public static int lcsIter(String a, String b){
+        int m = a.length();
+        int n = b.length();
+
+        int[][] arr = new int[m+1][n+1];
+
+        for(int i=0;i<m;i++){
+            arr[i][0] = 0;
+        }
+
+        for(int i=0;i<n;i++){
+            arr[0][n] = 0;
+        }
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(a.charAt(m-i)==b.charAt(n-j)){
+                    arr[i][j] = 1 + arr[i-1][j-1];
+                }
+                else{
+                    arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+                }
+            }
+        }
+
+        return arr[m][n];
+    }
+
+    public static void main(String[] args) {
+        String a = "aabbcc";
+        String b = "abc";
+
+        int m = a.length();
+        int n = b.length();
+        int[][] arr = new int[m+1][n+1];
+
+        for(int[] temp:arr){
+            Arrays.fill(temp, -1);
+        }
+
+        int ansRec = lcs(a, b, m, n, arr);
+        int ansIter = lcsIter(a, b);
+        System.out.println(ansIter);
+
+        
+    }
+    
+}
